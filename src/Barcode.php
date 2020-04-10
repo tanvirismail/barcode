bd46f46f7d4c952462205b6475155f817e0327cc
tanvir_ismail <t.ismail@geekpicker.com>
2020-04-10 12:06:37 +0600

    add builder

--- src/Barcode.php
+++ src/Barcode.php
@@ -4,7 +4,7 @@ namespace TanvirIsmail\Barcode;
 
 class Barcode 
 {
-    protected $type;
+    protected $type = 'code39';  // default
     protected $code;
     protected $barWidth = 2;
     protected $barColor = [[0, 0, 0],[255, 255, 255]];
@@ -73,130 +73,24 @@ class Barcode
     public function build()
     {
         if($this->type == 'code39'){
-            $code39 = new Code39($this->code);
-            
-            $barsCount = count($code39->barcode());
-            $padding = $this->barWidth * $this->margin;
-            $imageWidth = ($barsCount * $this->barWidth)+($padding*2);
-            $image = imagecreatetruecolor($imageWidth, $this->imageHeight);
-            imagesavealpha($image, true);
-
-            $color0 = imagecolorallocate($image, $this->barColor[1][0], $this->barColor[1][1], $this->barColor[1][2]);
-            $color1 = imagecolorallocate($image, $this->barColor[0][0], $this->barColor[0][1], $this->barColor[0][2] );
-            if(count($this->background) == 3){
-                $background = imagecolorallocate($image,$this->background[0], $this->background[1], $this->background[2]);
-            }
-            elseif(count($this->background) == 4){
-                $background = imagecolorallocatealpha($image,$this->background[0], $this->background[1], $this->background[2], $this->background[3]);
-            }
-            imagefill($image,0,0,$background);
-
-
-            // show code
-            if($this->showcode){
-                $xcenter = ( imagesx($image) - (strlen($this->code) * imagefontwidth(5) ) ) /2;
-                $y = imagesy($image)-$padding+2;
-                imagestring($image, 5, $xcenter, $y, $this->code, $color1);
-            }
-
-            // show label
-            if($this->label){
-                
-                $x = ( imagesx($image) - (strlen($this->label) * imagefontwidth(5) ) ) /2;
-                
-                if($this->labelPosition == 'left'){
-                    $x = $padding;
-                }
-                if($this->labelPosition == 'right'){
-                    $x = ( imagesx($image) - (strlen($this->label) * imagefontwidth(5) ) ) - $padding;
-                }
-                
-                $y = 2;
-                imagestring($image, 5, $x, $y, $this->label, $color1);
-            }
-
-            $barHeight = $this->imageHeight-$padding;
-            $position = $padding;
-            foreach ($code39->barcode() as $bit) {
-                $color = $bit == 0 ? $color0 : $color1;
-                imagefilledrectangle($image, $position , $barHeight, $position+$this->barWidth, $padding, $color);
-                $position += $this->barWidth;
-            }
-
-            ob_start();
-            if($this->fileExtension == 'png'){
-                imagepng($image);
-            }
-            if($this->fileExtension == 'jpeg'){
-                imagejpeg($image);
-            }
-            imagedestroy($image);
-            $imagedata = ob_get_clean();
-            return $imagedata;
+            $format = new Code39($this->code);
         }
-
-
         if($this->type == 'code128'){
-            $code128 = new Code128($this->code);
-            $barsCount = count($code128->barcode());
-            $padding = $this->barWidth * $this->margin;
-            $imageWidth = ($barsCount * $this->barWidth)+($padding*2);
-            $image = imagecreatetruecolor($imageWidth, $this->imageHeight);
-            imagesavealpha($image, true);
-
-            $color0 = imagecolorallocate($image, $this->barColor[1][0], $this->barColor[1][1], $this->barColor[1][2]);
-            $color1 = imagecolorallocate($image, $this->barColor[0][0], $this->barColor[0][1], $this->barColor[0][2] );
-            if(count($this->background) == 3){
-                $background = imagecolorallocate($image,$this->background[0], $this->background[1], $this->background[2]);
-            }
-            elseif(count($this->background) == 4){
-                $background = imagecolorallocatealpha($image,$this->background[0], $this->background[1], $this->background[2], $this->background[3]);
-            }
-            imagefill($image,0,0,$background);
-
-
-            // show code
-            if($this->showcode){
-                $xcenter = ( imagesx($image) - (strlen($this->code) * imagefontwidth(5) ) ) /2;
-                $y = imagesy($image)-$padding+2;
-                imagestring($image, 5, $xcenter, $y, $this->code, $color1);
-            }
-
-            // show label
-            if($this->label){
-                
-                $x = ( imagesx($image) - (strlen($this->label) * imagefontwidth(5) ) ) /2;
-                
-                if($this->labelPosition == 'left'){
-                    $x = $padding;
-                }
-                if($this->labelPosition == 'right'){
-                    $x = ( imagesx($image) - (strlen($this->label) * imagefontwidth(5) ) ) - $padding;
-                }
-                
-                $y = 2;
-                imagestring($image, 5, $x, $y, $this->label, $color1);
-            }
-
-            $barHeight = $this->imageHeight-$padding;
-            $position = $padding;
-            foreach ($code128->barcode() as $bit) {
-                $color = $bit == 0 ? $color0 : $color1;
-                imagefilledrectangle($image, $position , $barHeight, $position+$this->barWidth, $padding, $color);
-                $position += $this->barWidth;
-            }
-
-            ob_start();
-            if($this->fileExtension == 'png'){
-                imagepng($image);
-            }
-            if($this->fileExtension == 'jpeg'){
-                imagejpeg($image);
-            }
-            imagedestroy($image);
-            $imagedata = ob_get_clean();
-            return $imagedata;
+            $format = new Code128($this->code);
         }
+        $bulid = new Builder(
+            $format, 
+            $this->barWidth, 
+            $this->imageHeight, 
+            $this->barColor, 
+            $this->background, 
+            $this->showcode,
+            $this->label,
+            $this->fileExtension,
+            $this->margin,
+            $this->labelPosition
+        );
+        return $bulid->make();
     }
 
     public function path($path)
